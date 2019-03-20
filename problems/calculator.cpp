2ec2e9086d1b1b95f2499fa88af85b85c8e2776b
Artur <devilyoushy@gmail.com>
2019-03-20 22:37:26 +0000

    calculator
    initialized file for creating calculator

---
+++ problems/calculator.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <limits>
+
+int main()
+{
+  // placeholder for calculator
+
+  return 0;
+}
