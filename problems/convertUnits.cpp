76d7cd2949a55411f115b524db0c4f9dfbb51a54
Artur <devilyoushy@gmail.com>
2019-03-21 22:11:46 +0000

    Miles to Kilometers converter
    First assignment from Derek Banas' youtube course

--- problems/convertUnits.cpp
+++ problems/convertUnits.cpp
@@ -5,9 +5,38 @@
 #include <sstream>
 #include <limits>
 
+using namespace std;
+
 int main()
 {
   // km = miles * 1.60934
+
+  string provideValue("Provide value: ");
+  string sValue;
+
+  cout << provideValue;
+  getline(cin, sValue);
+
+  double value = stod(sValue);
+
+  string operation("Do you want to convert (M)iles to kilometers or (K)ilometers to miles?");
+  string option;
+
+  cout << operation << endl;
+  getline(cin, option);
+
+  if (option == "M")
+  {
+    printf("%.2f miles is %.2f kilometers", value, value * 0.62137);
+  }
+  else if (option == "K")
+  {
+    printf("%.2f kilomenters is %.2f miles", value, value * 1.60934);
+  }
+  else
+  {
+    printf("Well, you haven't provided the option...");
+  }
 
   return 0;
 }
